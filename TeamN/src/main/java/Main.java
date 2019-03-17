581d81368d1887ee27aabc14fb896c1524742d95
Nicole Cotto <nicolecotto316@gmail.com>
2019-03-17 15:07:42 -0400

    hi

--- TeamN/src/main/java/Main.java
+++ TeamN/src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Maya");
         System.out.println("Hello");
         System.out.println("Sam");
-        System.out.println("Nicole C");
+        System.out.println("Nicole");
         System.out.println("Nick Pingal");
         System.out.println("Mattheus Nascimento");
         System.out.println("Lauren Hatfield");
