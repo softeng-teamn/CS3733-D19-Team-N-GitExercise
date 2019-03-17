0d04a1eb87ace875cb96ea6bdde210a24921998a
mattheus98 <mcnascimento@wpi.edu>
2019-03-17 15:02:28 -0400

    Merge branch 'master' of https://github.com/softeng-teamn/CS3733-D19-Team-N-GitExercise
    # Conflicts:
    #	.idea/misc.xml
    #	TeamN/src/main/java/Main.java

--- TeamN/src/main/java/Main.java
+++ TeamN/src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nicole C");
-        System.out.println("Nick Pingal");
-        System.out.println("Mattheus Nascimento");
+        System.out.println("Branch-Mattheus Nascimento");
     }
 }
